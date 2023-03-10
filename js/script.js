82c618dd135deef000ef45a70897112c79510cd4
Angelop26 <angeloprospero26@gmail.com>
2023-03-10 14:02:06 +0100

    bonus 1 complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+const grid = document.getElementById('dom')
 for (let i = 1; i <= 100; i++){
+    let result
     if (i % 3 === 0 && i % 5 === 0){
-        console.log('fizzbuz');
+        result = ('fizzbuzz')
     } else if(i % 5 === 0) {
-        console.log('buzz');
+        result = ('buzz')
     } else if (i % 3 === 0){
-        console.log('fizz');
+        result = ('fizz')
     } else {
-        console.log(i);
+        result = i
     }
+
+    grid.innerHTML += `<div>${result}</div>`
 }
